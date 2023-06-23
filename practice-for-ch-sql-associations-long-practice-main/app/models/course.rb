e8194299f68d4b05cde76eaaacaf1a964a3a5dbe
harivmasoor <harimasoor@gmail.com>
2023-06-22 17:06:24 -0700

    associations

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -10,4 +10,24 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+    validates :name, presence: true
+
+    belongs_to :prereq,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course
+    
+    has_many :prereq_courses,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course
+
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+    
+    has_many :students, 
+        through: :enrollments,
+        source: :student
 end
